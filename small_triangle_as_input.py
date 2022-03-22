d475778d8b3ce933722c2cda4b07cd8190ecb823
gorkemguzel32 <97730415+gorkemguzel32@users.noreply.github.com>
2022-03-22 21:05:29 +0300

    Add files via upload

---
+++ small_triangle_as_input.py
@@ -0,0 +1,24 @@
+# Defining the triangle
+triangle = [[1], [8, 4], [2, 6, 9], [8, 5, 9, 3]]
+
+# Creating list of prime numbers until 1000 (The largest value in dataset is 999)
+primes = [i for i in range(2, 1000) if 0 not in [i % n for n in range(2, i)]]
+
+# Replacing prime numbers with a large negative number to make sure 
+# that path with the highest sum won't be a path involving prime number(s).
+for prime_num in primes:
+    for i, x in enumerate(triangle):
+        for j, a in enumerate(x):
+            if triangle[i][j] == prime_num:
+                triangle[i][j] = -32000
+
+
+# Function to find the path with the highest sum from bottom to the top.
+def find_path():
+    for i in reversed(range(len(triangle) - 1)):
+        for j in range(len(triangle[i])):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+    return str(triangle[0][0])
+
+
+print(find_path())
